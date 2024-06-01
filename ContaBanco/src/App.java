d17bc0e2257ba0a5dba67093cfbb4a4803fca650
Cayo Cesar <cayocesar652@gmail.com>
2024-06-01 11:56:08 -0300

    Final

--- ContaBanco/src/App.java
+++ ContaBanco/src/App.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o seu nome: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo da conta: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + " , obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " , conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque");
+    
+        scanner.close();
     }
 }
